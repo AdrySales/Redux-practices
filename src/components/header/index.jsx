0fb3070ada85dddb603cad7f660b9d6dc68c55da
AdrySales <yasmim.adrieny@gmail.com>
2024-12-11 14:50:46 -0300

    Adição da funcionalidade de aumentar a quantidade de produtos quando ele adicionado mais de uma vez no carrinho, tudo feito pelo redux

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {useSelector,useDispatch} from 'react-redux'; //redux
 // Components
 import Cart from "../cart/index";
+import React, { useMemo } from 'react';
 
 // Styles
 import * as Styles from "./styles";
@@ -13,9 +14,13 @@ function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
   const {currentUser} =useSelector((rootReducer)=>rootReducer.userReducer);//redux
+  const {products} =useSelector((rootReducer)=>rootReducer.cartReducer);//redux
   console.log(currentUser);//redux
   const dispatch =useDispatch();//redux
-
+  
+  const productsCount = useMemo(() => {
+    return products.reduce((acc,curr)=> acc + curr.quantity, 0  )
+  },[products])
   const handleCartClick = () => {
     setCartIsVisible(true);
   };
@@ -36,7 +41,7 @@ function Header() {
        {currentUser ? (<div onClick={handleLogout}>Sair</div>) :(<div onClick={handleLogin}>Login</div>)  //se clicar em logar ele seta as props para os dados que eu passei e se eu clicar em logout ele seta para null
 }
         
-        <div onClick={handleCartClick}>Carrinho</div>
+        <div onClick={handleCartClick}>Carrinho ({productsCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -7,7 +7,23 @@ const initialState = {
 const cartReducer = (state =initialState, action ) =>{
     switch (action.type){
         case CartActionTypes.ADD_PRODUCT:
-            return {...state, products: [...state.products , action.payload]}
+            //verificar se o produto ja esta no carrinho
+            const productIsAlreadyInCart = state.products.some(
+                (product) => product.id === action.payload.id
+            )
+            //se ele estiver deve aumentar a quantidade dele
+            if(productIsAlreadyInCart){
+                return {
+                    ...state, 
+                    products : state.products.map((product) =>
+                 product.id===action.payload.id 
+                    ? {...product, quantity: product.quantity + 1} : product
+                )
+                }
+            }
+            //se ele nao estiver adicona-lo
+            return {...state, products: [...state.products ,{...action.payload, quantity:1} ]}
+           
         default:
             return state
     }
