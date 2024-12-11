dafb5ade0b400f0edc040b4eee7a61d199f6ee18
AdrySales <yasmim.adrieny@gmail.com>
2024-12-11 13:52:14 -0300

    adição de todo o fluxo do reducer para a funcionalidade itens no carrinho

---
+++ src/redux/cart/reducer.js
@@ -0,0 +1,16 @@
+import CartActionTypes from "./actions-types";
+const initialState = {
+    products : [],
+    productsTotalPrice: 0
+};
+
+const cartReducer = (state =initialState, action ) =>{
+    switch (action.type){
+        case CartActionTypes.ADD_PRODUCT:
+            return {...state, products: [...state.products , action.payload]}
+        default:
+            return state
+    }
+
+}
+export default cartReducer;
