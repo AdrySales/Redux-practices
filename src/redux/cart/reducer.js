71e8bdbede49b5ac7e97677e39391f3ca61a4c1d
AdrySales <yasmim.adrieny@gmail.com>
2024-12-11 15:01:39 -0300

    Adição da funcionalidade de excluir itens do carrinho

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -23,7 +23,8 @@ const cartReducer = (state =initialState, action ) =>{
             }
             //se ele nao estiver adicona-lo
             return {...state, products: [...state.products ,{...action.payload, quantity:1} ]}
-           
+        case CartActionTypes.REMOVE_PRODUCT:
+            return {...state, products: state.products.filter( product => product.id !== action.payload)}
         default:
             return state
     }
